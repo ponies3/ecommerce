fe4bda7fa438c36fa7b268d41d7e2c31921d18b3
avallejo <avallejofornes@gmail.com>
2024-05-08 19:31:07 +0200

    feat: add validations to getRateByBrandAndProductAndApplicationDate

--- src/main/java/antoni/ecommerce/core/infraestructure/exceptions/ErrorsConstants.java
+++ src/main/java/antoni/ecommerce/core/infraestructure/exceptions/ErrorsConstants.java
@@ -1,4 +1,10 @@
 package antoni.ecommerce.core.infraestructure.exceptions;
 
 public final class ErrorsConstants {
+    public static final String BRAND_ID_IS_REQUIRED = "BrandId is required";
+    public static final String BRAND_ID_IS_REQUIRED_CODE = "RATES-001";
+    public static final String PRODUCT_ID_IS_REQUIRED = "ProductId is required";
+    public static final String PRODUCT_ID_IS_REQUIRED_CODE = "RATES-002";
+    public static final String APPLICATION_DATE_IS_REQUIRED = "ApplicationDate is required";
+    public static final String APPLICATION_DATE_IS_REQUIRED_CODE = "RATES-003";
 }
---
+++ src/main/java/antoni/ecommerce/rates/application/GetRateUseCase.java
@@ -0,0 +1,38 @@
+package antoni.ecommerce.rates.application;
+
+import antoni.ecommerce.core.infraestructure.exceptions.BusinessException;
+import antoni.ecommerce.core.infraestructure.exceptions.ErrorsConstants;
+import antoni.ecommerce.rates.domain.Rate;
+import antoni.ecommerce.rates.domain.RateQuery;
+import antoni.ecommerce.rates.domain.RatesRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class GetRateUseCase {
+    private final RatesRepository ratesRepository;
+
+    public GetRateUseCase(RatesRepository rateRepository) {
+        this.ratesRepository = rateRepository;
+    }
+
+    public Rate getRateByBrandAndProductAndApplicationDate(RateQuery rateQuery) throws BusinessException {
+        if (Objects.isNull(rateQuery.getBrandId())) {
+            throw new BusinessException(ErrorsConstants.BRAND_ID_IS_REQUIRED,
+                    ErrorsConstants.BRAND_ID_IS_REQUIRED_CODE, HttpStatus.BAD_REQUEST.value());
+        }
+        if (Objects.isNull(rateQuery.getProductId())) {
+            throw new BusinessException(ErrorsConstants.PRODUCT_ID_IS_REQUIRED ,
+                    ErrorsConstants.PRODUCT_ID_IS_REQUIRED_CODE, HttpStatus.BAD_REQUEST.value());
+        }
+        if (Objects.isNull(rateQuery.getApplicationDate())) {
+            throw new BusinessException(ErrorsConstants.APPLICATION_DATE_IS_REQUIRED,
+                    ErrorsConstants.APPLICATION_DATE_IS_REQUIRED_CODE, HttpStatus.BAD_REQUEST.value());
+        }
+        return ratesRepository.getRateByBrandAndProductAndApplicationDate( rateQuery.getBrandId(),
+                rateQuery.getProductId(),
+                rateQuery.getApplicationDate());
+    }
+}
